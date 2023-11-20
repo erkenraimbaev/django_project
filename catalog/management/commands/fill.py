dbba0ef19a03a54cdf0234888f78a1b6e93de5ec
erkenraimbaev <erken.orsk.2015@gmail.com>
2023-11-20 19:23:09 +0500

    add changes

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -6,18 +6,24 @@ from catalog.models import Product, Category
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        # Category.objects.all().delete()
+        category_list = [
+            {'name_category': 'Овощи', 'description': 'Едят все'},
+            {'name_category': 'Фрукты', 'description': 'Очень полезны'}
+        ]
+        Category.truncate_table_restart_id()
+        for category in category_list:
+            Category.objects.create(**category)
+
         product_list = [
             {'name_product': 'Яблоко', 'description': 'Зеленое', 'category': '2', 'price': '100'},
             {'name_product': 'Морковь', 'description': 'Оранжевая', 'category': '1', 'price': '30'},
             {'name_product': 'Банан', 'description': 'Из Эквадора', 'category': '2', 'price': '100'}
-
         ]
 
         products_for_create = []
+        Product.truncate_table_restart_id()
         for product in product_list:
             category_id = product.pop('category')
-            category = Category.objects.get(id=category_id)
-            product['category'] = category
-            products_for_create.append(Product(**product))
+            products_for_create.append(Product(category_id=category_id, **product))
         Product.objects.bulk_create(products_for_create)
-
